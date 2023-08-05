fe7ceb3addc73abcb186343b32bebf1fcbb6d03e
Shunsei <140191667+shundev23@users.noreply.github.com>
2023-08-05 10:30:40 +0900

    トップページ微修正

--- lib/views.py
+++ lib/views.py
@@ -1,4 +1,14 @@
 from django.views.generic import TemplateView
+from Taberu.models import Taberu
 
 class IndexTemplateView(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['taberu_top'] = Taberu.objects.order_by('?')[0]
+        context['taberu_list'] = Taberu.objects.order_by("-created")
+
+        return context
+
