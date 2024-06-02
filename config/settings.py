4d9cda153441ce93f5538571b0451bae34ca9dd7
shunsei <shunsei.nishimiya@gmail.com>
2024-06-02 09:56:30 +0900

    Reconstruction for docker

--- config/settings.py
+++ config/settings.py
@@ -83,14 +83,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'TaberuMaster',
-        # 'USER': 'postgres',
-        # 'PASSWORD': os.environ.get('DB_PASSWORD'),
-        # 'HOST': 'localhost',
-        # 'PORT': '5432',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TaberuMaster',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
